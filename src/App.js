7417a486180d4d0b7d28902df32cb0f62bd05c13
hmassareli <henrique.massareli@gmail.com>
2022-03-11 10:57:39 -0300

    feat: solution

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import React from 'react';
-import './App.css';
-import 'h8k-components';
+import React, { useState } from "react";
+import "./App.css";
+import "h8k-components";
 
-import Articles from './components/Articles';
+import Articles from "./components/Articles";
 
 const title = "Sorting Articles";
 
-function App({articles}) {
+function App({ articles }) {
+  const [articleSort, setArticleSort] = useState("mostUpvotedArticles");
 
-    return (
-        <div className="App">
-            <h8k-navbar header={title}></h8k-navbar>
-            <div className="layout-row align-items-center justify-content-center my-20 navigation">
-                <label className="form-hint mb-0 text-uppercase font-weight-light">Sort By</label>
-                <button data-testid="most-upvoted-link" className="small">Most Upvoted</button>
-                <button data-testid="most-recent-link" className="small">Most Recent</button>
-            </div>
-            <Articles articles={articles}/>
-        </div>
-    );
-
+  return (
+    <div className="App">
+      <h8k-navbar header={title}></h8k-navbar>
+      <div className="layout-row align-items-center justify-content-center my-20 navigation">
+        <label className="form-hint mb-0 text-uppercase font-weight-light">
+          Sort By
+        </label>
+        <button
+          data-testid="most-upvoted-link"
+          onClick={() => setArticleSort("mostUpvotedArticles")}
+          className="small"
+        >
+          Most Upvoted
+        </button>
+        <button
+          data-testid="most-recent-link"
+          onClick={() => setArticleSort("mostRecentArticles")}
+          className="small"
+        >
+          Most Recent
+        </button>
+      </div>
+      <Articles articles={articles} sort={articleSort} />
+    </div>
+  );
 }
 
 export default App;
--- src/components/Articles.js
+++ src/components/Articles.js
@@ -1,28 +1,63 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 
-function Articles() {
+function Articles({ articles, sort }) {
+  const deepCopy = (array) => JSON.parse(JSON.stringify(array));
 
-    return (
-        <div className="card w-50 mx-auto">
-            <table>
-                <thead>
-                <tr>
-                    <th>Title</th>
-                    <th>Upvotes</th>
-                    <th>Date</th>
-                </tr>
-                </thead>
-                <tbody>
-                <tr data-testid="article" key="article-index">
-                    <td data-testid="article-title">Article 1 title</td>
-                    <td data-testid="article-upvotes">Article 1 title</td>
-                    <td data-testid="article-date">Article 1 title</td>
-                </tr>
-                </tbody>
-            </table>
-        </div>
-    );
+  const [sortedArticles, setSortedArticles] = useState(articles);
 
+  const compareDates = (a, b) => {
+    if (new Date(b.date) < new Date(a.date)) {
+      return -1;
+    }
+    if (new Date(b.date) > new Date(a.date)) {
+      return 1;
+    }
+    return 0;
+  };
+  const compareNumbers = (a, b) => {
+    return b.upvotes - a.upvotes;
+  };
+
+  const showMostRecentArticles = () => {
+    const result = deepCopy(sortedArticles).sort(compareDates);
+    setSortedArticles(result);
+  };
+
+  const showMostUpvotedArticles = () => {
+    const result = deepCopy(sortedArticles).sort(compareNumbers);
+    setSortedArticles(result);
+  };
+
+  useEffect(() => {
+    if (sort === "mostUpvotedArticles") {
+      showMostUpvotedArticles();
+      return;
+    }
+    showMostRecentArticles();
+  }, [sort]);
+
+  return (
+    <div className="card w-50 mx-auto">
+      <table>
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Upvotes</th>
+            <th>Date</th>
+          </tr>
+        </thead>
+        <tbody>
+          {sortedArticles.map((item, i) => (
+            <tr data-testid="article" key={`article-index-${i}`}>
+              <td data-testid="article-title">{item.title}</td>
+              <td data-testid="article-upvotes">{item.upvotes}</td>
+              <td data-testid="article-date">{item.date}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 export default Articles;
